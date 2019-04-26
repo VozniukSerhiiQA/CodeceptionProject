39836eadfd87631dddf704a594a0b5d53a0e3a21
Serhii Vozniuk <sergeyv1986@gmail.com>
2019-04-26 10:09:52 +0200

    In a middle of creating 05 test. Try to get data from other tests and reuse it

---
+++ tests/_support/CollapsedSteps/RememberValues.php
@@ -0,0 +1,11 @@
+<?php
+namespace CollapsedSteps;
+
+class RememberValues
+{
+	public static function TODO (\AcceptanceTester $I)
+    {
+    	//test class for grabbed values
+    }
+		
+}
--- tests/_support/Objects/Banners.php
+++ tests/_support/Objects/Banners.php
@@ -3,7 +3,7 @@ namespace Objects;
 
 class Banners
 {
-	 public static $Banner1 = '//div[@class=\'owl-item active\']//div[contains(@style,\'/medias/ROAD-2553-Home-Hero-1440x810.jpg\')]';
-	 public static $Banner2 = '//div[@class=\'owl-item active\']//div[contains(@style,\'/medias/IMG-9749.jpg\')]';
-	 public static $Banner3 = '//div[@class=\'owl-item active\']//div[contains(@style,\'/medias/sagan-collection-Home-Hero-UPDATE-1440x810.jpg\')]';
+	 public static $Banner1 = "//div[@class='owl-item active']//div[contains(@style,'/medias/ROAD-2553-Home-Hero-1440x810.jpg')]";
+	 public static $Banner2 = "//div[@class='owl-item active']//div[contains(@style,'/medias/IMG-9749.jpg')]";
+	 public static $Banner3 = "//div[@class='owl-item active']//div[contains(@style,'/medias/sagan-collection-Home-Hero-UPDATE-1440x810.jpg')]";
 }
